e8a0e6b525f46ba3439bb488a584c63b6ddbcb31
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-02-06 00:09:49 +0900

    Update List Handler

--- 003/sam-nodejs-rest/demo-app-list/app.js
+++ 003/sam-nodejs-rest/demo-app-list/app.js
@@ -1,9 +1,9 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
+const axios = require('axios')
+const url = 'http://checkip.amazonaws.com/';
 let response;
 
 // Get the DynamoDB table name from environment variables
-const tableName = process.env.SAMPLE_TABLE;
+const tableName = process.env.ECHO_TABLE;
 
 // Create a DocumentClient that represents the query to add an item
 const dynamodb = require('aws-sdk/clients/dynamodb');
@@ -11,18 +11,19 @@ const docClient = new dynamodb.DocumentClient();
 
 exports.lambdaHandler = async (event, context) => {
     try {
-        // const ret = await axios(url);
+        const ret = await axios(url);
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
                 message: 'List abra kadabra',
-                // location: ret.data.trim()
+                location: ret.data.trim(),
+                dynamotable: tableName
             })
-        }
+        };
     } catch (err) {
         console.log(err);
         return err;
     }
 
-    return response
+    return response;
 };
