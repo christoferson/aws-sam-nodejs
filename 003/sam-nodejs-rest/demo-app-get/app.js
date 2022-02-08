5e3b157d45fdf1dc3f223b7d1d6ea0340effa049
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-02-08 23:40:22 +0900

    Add common function in Lambda Layer

--- 003/sam-nodejs-rest/demo-app-get/app.js
+++ 003/sam-nodejs-rest/demo-app-get/app.js
@@ -8,6 +8,9 @@ const tableName = process.env.ECHO_TABLE;
 const dynamodb = require('aws-sdk/clients/dynamodb');
 const docClient = new dynamodb.DocumentClient();
 
+const mydateutils = require("/opt/nodejs/mydateutils");
+const getAddressFromZipCode = require("/opt/nodejs/lib/getAddressFromZipCode");
+
 exports.lambdaHandler = async (event, context) => {
 
     if (event.httpMethod !== 'GET') {
@@ -33,12 +36,18 @@ exports.lambdaHandler = async (event, context) => {
         return err;
     }
 
+
+    let x = await getAddressFromZipCode("8u3453");
+    let y = await mydateutils.currentDate();
+
     try {
         // const ret = await axios(url);
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
                 message: 'Get abra kadabra',
+                layerfunction: x,
+                layerfunctiony: y,
                 // location: ret.data.trim()
                 item: item
             })
---
+++ 003/sam-nodejs-rest/demo-cmn-layer/lib/getAddressFromZipCode.js
@@ -0,0 +1,5 @@
+module.exports = async (zipcode) => {
+    let result = "Called with " + zipcode;
+    console.log(result);
+    return result;
+ };
