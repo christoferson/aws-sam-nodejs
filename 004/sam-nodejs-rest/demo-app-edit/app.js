a9c8e2cee997902e0bd931a514ab77b39cbb61e0
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-02-19 17:40:05 +0900

    Edit - Temp Implementation

--- 004/sam-nodejs-rest/demo-app-edit/app.js
+++ 004/sam-nodejs-rest/demo-app-edit/app.js
@@ -1,33 +1,57 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
 let response;
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
+const tableName = process.env.ECHO_TABLE;
+
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
+
 exports.lambdaHandler = async (event, context) => {
+
+    if (event.httpMethod !== 'POST') {
+        throw new Error(`Edit only accepts POST method. Input: ${event.httpMethod}`);
+    }
+
+    const id = event.pathParameters.id;
+    
+    console.log(`Edit Item. ID=${id}`);
+
+    let item = {};
+
     try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'Edit abra kadabra',
-                // location: ret.data.trim()
-            })
-        }
+
+        var params = {
+            TableName : tableName,
+            Key: { id: id },
+          };
+          const data = await docClient.get(params).promise();
+          item = data.Item;
+          
+        console.log(`Edit Item. Data=${item}`);          
+
     } catch (err) {
         console.log(err);
         return err;
     }
+    
+    if (item === undefined) {
+        console.log(`Edit Item. Item ${id} not found.`);
+        response = {
+            'statusCode': 401,
+            'body': JSON.stringify({
+                message: `Edit - Unregistered Item. ID=${id}`
+            })
+        };        
+    } else {
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: `Edit - Item Modified. ID=${id}`,
+                item: item
+            })
+        };
+    }
 
-    return response
+    return response;
+
+    
 };
